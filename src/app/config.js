310fe6b6ca2ad6cb565cd383dd2f3f4ee85636aa
iamzjt-front-end <iamzjt@foxmail.com>
2022-05-02 23:11:12 +0800

    database

--- src/app/config.js
+++ src/app/config.js
@@ -2,4 +2,6 @@ const dotenv = require("dotenv");
 
 dotenv.config({
   path: ".env.dev"
-});
+})
+
+console.log(process.env);
--- src/app/database.js
+++ src/app/database.js
@@ -1,6 +1,21 @@
 const mysql = require("mysql2");
 
-mysql.createPool({
+const connections = mysql.createPool({
   host: process.env.MYSQL_HOST,
-
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
+
+connections.getConnection((err, conn) => {
+  conn.connect(err => {
+    if (err) {
+      console.log("数据库连接失败: ", err);
+    } else {
+      console.log("数据库连接成功~");
+    }
+  })
+})
+
+module.exports = connections.promise();
