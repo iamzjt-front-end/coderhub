d76a2f8338330ed9992b3f1acb33491aea27e02c
iamzjt-front-end <iamzjt@foxmail.com>
2022-05-02 23:56:55 +0800

    database连接测试

--- src/app/config.js
+++ src/app/config.js
@@ -3,5 +3,3 @@ const dotenv = require("dotenv");
 dotenv.config({
   path: ".env.dev"
 })
-
-console.log(process.env);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const app = require("./app");
 require("./app/config");
+require("./app/database");
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`服务器在${ process.env.APP_PORT }端口启动成功~`);
