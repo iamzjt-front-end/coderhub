ad76ab283cd39936ae093f32fa542312d7b376d6
iamzjt-front-end <iamzjt@foxmail.com>
2022-05-03 00:12:35 +0800

    database连接测试

--- src/app/database.js
+++ src/app/database.js
@@ -3,15 +3,15 @@ const mysql = require("mysql2");
 const connections = mysql.createPool({
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
+  database: process.env.MYSQL_DATABASE,
   user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  password: process.env.MYSQL_PASSWORD
 });
 
 connections.getConnection((err, conn) => {
-  conn.connect(err => {
-    if (err) {
-      console.log("数据库连接失败: ", err);
+  conn.connect(err1 => {
+    if (err1) {
+      console.log("数据库连接失败: ", err1);
     } else {
       console.log("数据库连接成功~");
     }
