2457fd9e8b5f9557b3bf494b6d29182e299a42bc
iamzjt-front-end <iamzjt@foxmail.com>
2022-05-03 16:30:39 +0800

    写入数据库

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -3,7 +3,7 @@ const connection = require("../app/database");
 class UserService {
   async create(user) {
     const { name, password } = user;
-    const statement = `INSERT INTO \`user\` (name, password) VALUES (?, ?);`;
+    const statement = `INSERT INTO user (name, password) VALUES (?, ?);`;
 
     // 将user存储到数据库中
     return connection.execute(statement, [name, password]);
