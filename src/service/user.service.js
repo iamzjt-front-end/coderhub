841bd5a5de41b907cf84e57314654a579ce5161a
iamzjt-front-end <iamzjt@foxmail.com>
2022-05-05 17:16:55 +0800

    优化一下

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -6,7 +6,7 @@ class UserService {
     const { name, password } = user;
     const statement = `INSERT INTO user (name, password) VALUES (?, ?);`;
 
-    const result = connection.execute(statement, [name, password]);
+    const result = await connection.execute(statement, [name, password]);
     return result;
   }
 
@@ -14,7 +14,7 @@ class UserService {
   async getUserByName(name) {
     const statement = `SELECT * FROM user WHERE name = ?;`;
 
-    const result = connection.execute(statement, [name]);
+    const result = await connection.execute(statement, [name]);
     return result[0];
   }
 }
