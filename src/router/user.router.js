8a20546e22f8456ad81ba3eb2d9d3c818fff2ec1
iamzjt-front-end <iamzjt@foxmail.com>
2022-05-14 17:10:33 +0800

    优化一下

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -5,7 +5,7 @@ const Router = require("koa-router");
 const { create } = require("../controller/user.controller");
 const { verifyUser, handlePassword } = require("../middleware/user.middleware");
 
-const userRouter = new Router({ prefix: "/users" });
+const userRouter = new Router({ prefix: "/user" });
 
 userRouter.post("/", verifyUser, handlePassword, create);
 
