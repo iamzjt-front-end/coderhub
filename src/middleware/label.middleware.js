9f536f39642523a4700d81fe68aee04042745da9
iamzjt <iamzjt@foxmail.com>
2022-05-31 23:21:17 +0800

    中间件 verifyLabelExists 开发

--- src/middleware/label.middleware.js
+++ src/middleware/label.middleware.js
@@ -1,11 +1,24 @@
 /**
  * 标签
  */
-const errorTypes = require("../constants/error-types");
+const labelService = require("../service/label.service");
 
 const verifyLabelExists = async (ctx, next) => {
-  // 1. 获取标签
+  // 1. 获取需要添加的所有的标签
   const { labels } = ctx.request.body;
+
+  // 2. 判断每一个标签在 label 表中是否存在
+  for (let name of labels) {
+    const labelResult = await labelService.getLabelByName(name);
+    const label = { name };
+    if (labelResult.length) {
+      label.id = labelResult[0].id;
+    } else {
+      // 如果不存在就创建
+      const result = await labelService.create(name);
+      label.id = result.insertId;
+    }
+  }
 
   await next();
 }
--- src/service/label.service.js
+++ src/service/label.service.js
@@ -10,6 +10,13 @@ class LabelService {
     const result = await connection.execute(statement, [name]);
     return result[0];
   }
+
+  async getLabelByName(name) {
+    const statement = `SELECT * FROM label WHERE name = ?;`;
+
+    const result = await connection.execute(statement, [name]);
+    return result[0];
+  }
 }
 
 module.exports = new LabelService();
