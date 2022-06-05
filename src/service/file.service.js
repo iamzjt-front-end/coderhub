c49aaba30e570039fac3a16edfc7c788314db586
iamzjt-front-end <iamzjt@foxmail.com>
2022-06-05 15:34:40 +0800

    优化

--- src/service/file.service.js
+++ src/service/file.service.js
@@ -12,14 +12,14 @@ class FileService {
   }
 
   async getAvatarByUserId(userId) {
-    const statement = `SELECT * FROM file WHERE user_id = ?;`;
+    const statement = `SELECT * FROM file WHERE user_id = ? AND dynamic_id IS NULL;`;
 
     const result = await connection.execute(statement, [userId]);
     return result[0][0];
   }
 
   async getFileByFilename(filename) {
-    const statement = `SELECT * FROM file WHERE filename = ?;`;
+    const statement = `SELECT * FROM file WHERE filename = ? WHERE dynamic_id IS NOT NULL;`;
 
     const result = await connection.execute(statement, [filename]);
     return result[0][0];
